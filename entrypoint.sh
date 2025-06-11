a095328c34c4f7c4fc11da921d8d59123bce497e
sebastian <sebastiannovales@gmail.com>
2025-06-10 21:20:10 -0300

    solucion de bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Generando env-config.js..."
+
 # Generar el archivo env-config.js con las variables de entorno
 echo "window.env = {" > /usr/share/nginx/html/env-config.js
 for var in $(env | grep VITE_); do
@@ -9,5 +11,9 @@ for var in $(env | grep VITE_); do
 done
 echo "};" >> /usr/share/nginx/html/env-config.js
 
+echo "Contenido de env-config.js:"
+cat /usr/share/nginx/html/env-config.js
+
+echo "Iniciando Nginx..."
 # Iniciar Nginx
 exec nginx -g "daemon off;"
